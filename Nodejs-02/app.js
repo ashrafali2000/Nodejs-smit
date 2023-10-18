d55033a6f3ab2775c5ce65326c8da0fc575365ea
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-18 11:35:39 +0500

    create middlerware and request data

--- Nodejs-02/app.js
+++ Nodejs-02/app.js
@@ -1,3 +1,24 @@
 const express = require("express");
 const app = express();
+
+// Middlerwares
+
+//First Middlerware
+app.use((req, res, next) => {
+    // console.log(req.url)
+    req.data = "ashraf ali"
+    console.log(req)
+    next()
+})
+
+//Second Middlerware
+app.use((req, res, next) => {
+    res.send(req.data)
+})
+
+
+
+
+
+
 app.listen(3000)
