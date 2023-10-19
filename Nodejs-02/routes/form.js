740fbf2a73c3ff019d9ac41cad1666b59bb6a817
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-19 10:38:29 +0500

    add writefile and readfile code

--- Nodejs-02/routes/form.js
+++ Nodejs-02/routes/form.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const path = require("path")
+const fs = require("fs");
 const router = express.Router();
 const formFile = path.join(process.cwd(), "views", "form.html");
+const storeDataFile = path.join(process.cwd(),"storeData", "data.json" );
 // /form/   (/form in appjs || / in the below)
 router.get("/", (req, res) => {
 res.sendFile(formFile);
 })
 router.post("/submit", (req, res) => {
-res.send(req.body)
-console.log(req.body)
+    fs.readFile(storeDataFile,"utf8",(err, datafromDatafile) => {
+        const data = req.body;
+        const oldData = JSON.parse(datafromDatafile);
+        const newData = JSON.stringify([oldData, data])
+        fs.writeFile(storeDataFile,newData,(err) => {
+            if(err) {
+                res.send("Data Crashed");
+            }
+            res.send("Data Recieved Sucessfully")
+            console.log(datafromDatafile)
+        })
+    })
 })
 
 module.exports = router;
