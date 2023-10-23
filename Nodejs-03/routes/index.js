53b3ac1a3ef96b9f0eab484b4aaa417286404300
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-23 15:08:36 +0500

    create dynamic route controller

--- Nodejs-03/routes/index.js
+++ Nodejs-03/routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
- res.send("Hello from  Controller")
+res.send("Hello from  Controller")
+})
+
+// Dynamic route
+router.get("/:categories", (req, res) => {
+res.send(req.params.categories)
 })
 
 module.exports = router;
