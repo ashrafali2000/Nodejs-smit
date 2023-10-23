1d0e38d928a9ced1c0c8d4a46e3a04e296720c9d
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-23 09:32:53 +0500

    create middleware and controller code

--- Nodejs-03/app.js
+++ Nodejs-03/app.js
@@ -1,6 +1,18 @@
 const express = require("express")
+const indexRoute = require("./routes/index")
 const app = express();
 const port = 3000;
+
+
+// MidddleWare
+app.get("/index", (req, res) => {
+res.send("Hello from middlerWare")
+})
+
+// Controller
+app.use("/index", indexRoute);
+
+
 
 app.listen(port, (req, res) => {
     console.log(`Server listen on Port ${port}`)
