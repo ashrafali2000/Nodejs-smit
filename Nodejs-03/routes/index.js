ec85b967a8ef1ba94c4e27de3b68979178382053
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-23 16:33:53 +0500

    add products array

--- Nodejs-03/routes/index.js
+++ Nodejs-03/routes/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const prodcutsFile = path.join(process.cwd(), "views","prducts.html");
 
-router.get("/", (req, res) => {
-    
-// res.send("Hello from  Controller")
-res.send(req.body)
-})
+// router.get("/", (req, res) => {
+// // res.send("Hello from  Controller")
+// res.send(req.body)
+// })
 
 // Dynamic route
-router.get("/:categories", (req, res) => {
-res.send(req.params.categories)
-})
+// router.get("/:categories", (req, res) => {
+// res.send(req.params.categories)
+// })
 
+router.get("/", (req, res) => {
+    res.sendFile(prodcutsFile);
+})
 
 
 module.exports = router;
