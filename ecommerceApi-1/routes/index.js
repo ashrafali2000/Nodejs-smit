6ab79dd909cfff743d875fe6a0f61c956cd6a4a1
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-24 15:24:37 +0500

    create project with files

---
+++ ecommerceApi-1/routes/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const prodcutsFile = path.join(process.cwd() ,"views","porducts.html");
+
+// router.get("/", (req, res) => {
+// // res.send("Hello from  Controller")
+// res.send(req.body)
+// })
+
+// Dynamic route
+// router.get("/:categories", (req, res) => {
+// res.send(req.params.categories)
+// })
+
+router.get("/", (req, res) => {
+    res.sendFile(prodcutsFile);
+})
+
+
+
+
+module.exports = router;
