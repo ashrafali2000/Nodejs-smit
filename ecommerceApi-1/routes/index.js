ddd6f1391e7022f08d625bc875eda3599a2834f2
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-25 12:55:49 +0500

    update prodcut views path

--- ecommerceApi-1/routes/index.js
+++ ecommerceApi-1/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const prodcutsFile = path.join(process.cwd() ,"views","porducts.html");
+const prodcutsFile = path.join(process.cwd() ,"views","products.html");
 
 // router.get("/", (req, res) => {
 // // res.send("Hello from  Controller")
