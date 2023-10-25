5dd2232e7bdfd71381d99cac4f176c7a02452163
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-25 11:50:07 +0500

    post product routes

--- ecommerceApi-1/routes/products.js
+++ ecommerceApi-1/routes/products.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const products = require("../data/products.json")
+const products = require("../data/products.json");
+const addProduct = require("../controller/product");
 
 router.get("/", (req, res) => {
     res.send(products)
+})
+
+router.post("/", (req, res) => {
+    const data = req.body;
+   addProduct(data);
+   console.log(data)
+   console.log(req.body)
+    res.status(200).send({status:200,message: "Product added sucessfully"});
 })
 
 module.exports = router
