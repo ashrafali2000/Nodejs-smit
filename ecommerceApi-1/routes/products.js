3916d0aff93deebca09737f255e2b86cb5e74dc4
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-25 12:55:28 +0500

    update productfile path

--- ecommerceApi-1/routes/products.js
+++ ecommerceApi-1/routes/products.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path")
 const products = require("../data/products.json");
 const addProduct = require("../controller/product");
+
+const productFile = path.join(process.cwd() ,"views","newProduct.html");
 
 router.get("/", (req, res) => {
     res.send(products)
 })
 
+
+router.get("/add", (req, res) => {
+    res.sendFile(productFile)
+})
+
 router.post("/", (req, res) => {
-    const data = req.body;
-   addProduct(data);
-   console.log(data)
+    const {title,description,price,discountPercentage,rating,stock,brands,category,thumbnail,images} = req.body;
+   addProduct({title,description,price,discountPercentage,rating,stock,brands,category,thumbnail,images:[images]});
+
    console.log(req.body)
     res.status(200).send({status:200,message: "Product added sucessfully"});
 })
