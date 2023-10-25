d9ea0578d2d9bd2f9b72e26eadc0d69e11e295d7
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-25 11:49:17 +0500

    create product controller

---
+++ ecommerceApi-1/controller/product.js
@@ -0,0 +1,19 @@
+const path = require("path")
+const fs = require("fs");
+const product = path.join(process.cwd(),"data", "products.json");
+
+const addProduct = (data) => {
+    fs.readFile(product, "utf8", (err, userData) => {
+        let myData = JSON.parse(userData);
+        let {products} = myData;
+        products.push(data)
+        let newData = JSON.stringify({products})
+        fs.writeFile(product, newData, (err) => {
+            if(err) {
+                console.log(users);
+            }
+        })
+
+    })
+}
+module.exports = addProduct
