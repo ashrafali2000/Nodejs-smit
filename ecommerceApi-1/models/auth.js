d8dfddf5f83ec12969d0dc335f5aff7ffb650868
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-30 16:21:58 +0500

    del model aukthsjs

--- ecommerceApi-1/models/auth.js
+++
@@ -1,9 +0,0 @@
-const { createUsers } = require("../controller/user")
-const createUser = async(firstName, lastName, email, password) => {
-try{
-  return await createUsers(firstName, lastName, email, password)
-}catch(err){
- throw err
-}
-}
-module.exports = {createUser}
